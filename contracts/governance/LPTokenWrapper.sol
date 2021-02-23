b23bb94b04447eeab3d84e95ce4ff3986020b8bd
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-02-23 21:14:04 +0100

    Set external contracts address via constructor

--- contracts/governance/LPTokenWrapper.sol
+++ contracts/governance/LPTokenWrapper.sol
@@ -6,18 +6,18 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
-/**
- * TO-DO:
- *   - Update BALLE contract address
- */
 contract LPTokenWrapper {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC20 public balle = IERC20(0xCa3F508B8e4Dd382eE878A314789373D80A5190A);
+    IERC20 public balle;
 
     uint256 private _totalSupply;
     mapping(address => uint256) private _balances;
+
+    constructor (address _balle) {
+        balle = IERC20(_balle);
+    }
 
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
