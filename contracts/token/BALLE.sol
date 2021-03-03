fa6a79ce4abdc3a767900329943803d0b8c4c408
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-03-03 18:21:37 +0100

    Cleanup token contract

--- contracts/token/BALLE.sol
+++ contracts/token/BALLE.sol
@@ -2,18 +2,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.4;
 
-import "@openzeppelin/contracts/GSN/Context.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 contract BALLE is ERC20 {
-    using SafeERC20 for IERC20;
-    using Address for address;
-    using SafeMath for uint256;
-
     address public governance;
     mapping(address => bool) public minters;
 
