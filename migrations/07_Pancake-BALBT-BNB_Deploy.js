1511570977f2d4b2b440e3f97cd08dbf7e2a391e
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-03-04 21:06:50 +0100

    Create first strategy

--- migrations/07_Pancake-BALBT-BNB_Deploy.js
+++ migrations/07_Pancake-BALBT-BNB_Deploy.js
@@ -14,7 +14,11 @@ module.exports = async function (deployer, network, accounts) {
   const pancakeRouterAddress = networkConfig.PancakeRouterAddress;
   const masterchefAddress = networkConfig.MasterchefAddress;
   const vaultRewardPoolAddress = networkConfig.vaultRewardPoolAddress;
+  const treasuryAddress = networkConfig.treasuryAddress;
+  const rewardPoolAddress = networkConfig.rewardPoolAddress;
   const balle = networkConfig.BALLE;
+  const wbnb = networkConfig.WBNB;
+  const cake = networkConfig.CAKE;
 
   if (network == 'bsc_mainnet') {
   } else {
@@ -23,30 +27,25 @@ module.exports = async function (deployer, network, accounts) {
     let strategyAddress = ethJsUtil.bufferToHex(ethJsUtil.generateAddress(accounts[0], nonce + 1));
     console.log('ADDRESS: ', strategyAddress)
 
-    let ballePancakeBALBT_BNB = await deployer.new(
-      BalleRewardedVaultV1, pancakePairAddress, strategyAddress, 
+    let ballePancakeBALBT_BNB = await deployer.new(BalleRewardedVaultV1, 
+      pancakePairAddress, strategyAddress, 
       'Balle Pancake bALBT-BNB', 'ballePancakeBALBT-BNB', 60,
       balle, vaultRewardPoolAddress, 100
     );
-    console.log(ballePancakeBALBT_BNB === undefined ? "UNDEFINED!!??" : "OK");
-    /*
-    // de nuevo lo mismo, el primer deployer.new(...) devuelve undefined!!
-    if (ballePancakeBALBT_BNB === undefined) {
+    console.log(ballePancakeBALBT_BNB === undefined ? 'UNDEFINED!!??' : 'OK');
 
-      nonce = await web3.eth.getTransactionCount(accounts[0])
-      console.log('NONCE: ', nonce)
-      strategyAddress = ethJsUtil.bufferToHex(ethJsUtil.generateAddress(accounts[0], nonce + 1));
-      console.log('ADDRESS: ', strategyAddress)
-
-      ballePancakeBALBT_BNB = await deployer.new(
-        BalleRewardedVaultV1, pancakePairAddress, strategyAddress, 
-        'Balle Pancake bALBT-BNB', 'ballePancakeBALBT-BNB', 60,
-        balle, vaultRewardPoolAddress, 100
-      );
-      console.log(ballePancakeBALBT_BNB === undefined ? "UNDEFINED!!??" : "OK");
-    }*/
     networkConfig['ballePancakeBALBT_BNB'] = ballePancakeBALBT_BNB.address;
 
+    let stratPancakeBALBT_BNB = await deployer.new(CakeLPStrategyV1,
+      wbnb, balle, cake, pancakeRouterAddress, masterchefAddress,
+      pancakePairAddress, 103, rewardPoolAddress, treasuryAddress,
+      ballePancakeBALBT_BNB.address
+    );
+    console.log(ballePancakeBALBT_BNB === undefined ? 'UNDEFINED!!??' : 'OK');
+    console.log('STRAT. ADDRESS: ', stratPancakeBALBT_BNB.address)
+
+    networkConfig['stratPancakeBALBT_BNB'] = stratPancakeBALBT_BNB.address;
+    fs.writeFileSync(networkConfigFilename, JSON.stringify(networkConfig, null, 2), { flag: 'w' });
   }
 
 };
