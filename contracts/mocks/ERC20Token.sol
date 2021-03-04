4b617c2e710e9ff00217185f05b616a7a357539e
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-03-04 09:57:42 +0100

    Create ERC20Token mock

--- contracts/mocks/ERC20Token.sol
+++ contracts/mocks/ERC20Token.sol
@@ -1,12 +1,12 @@
-// contracts/mocks/TKNA.sol
+// contracts/mocks/ERC20Token.sol
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TKNA is ERC20 {
+contract ERC20Token is ERC20 {
 
-    constructor() ERC20("Token A", "TKNA") {
+    constructor() ERC20(string memory name_, string memory symbol_) {
         _mint(msg.sender, 100 * (10 ** uint256(decimals())));
     }
 
