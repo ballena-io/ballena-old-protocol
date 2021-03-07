4d2ddce7698a7016d050a4ed5e0678bcd47439a2
Jose F. Fernandez <jffernandez@fenix-es.com>
2021-03-07 14:13:17 +0100

    Add staking tests to RewardPool

--- test/rewardPool.ts
+++ test/rewardPool.ts
@@ -79,4 +79,88 @@ contract('RewardPool', ([owner, account1, account2, account3, account4]) => {
 
     })
 
+    describe('Test staking of BALLE', () => {
+        let rewardPoolInstance: RewardPoolInstance
+        let balleInstance: BALLEInstance
+
+        beforeEach(async () => {
+            // To deploy a new contract for each test, use .new() instead of .deployed()
+            rewardPoolInstance = await RewardPool.deployed()
+            balleInstance = await BALLE.deployed()
+        })
+
+        it('should accept BALLE staking', async () => {
+            await balleInstance.addMinter(owner);
+            await balleInstance.mint(owner, toBN(10e18))
+            await balleInstance.mint(account1, toBN(5e18))
+
+            // Stake 5 BALLE from owner
+            await balleInstance.approve(rewardPoolInstance.address, toBN(5e18))
+            await rewardPoolInstance.stake(toBN(5e18))
+            // Stake 5 BALLE from account 1
+            await balleInstance.approve(rewardPoolInstance.address, toBN(5e18), {from: account1})
+            await rewardPoolInstance.stake(toBN(5e18), {from: account1})
+            // Get BALLE balance on contract
+            const rewardPoolBalance = await balleInstance.balanceOf(rewardPoolInstance.address)
+            // Get staked BALLE tokens
+            const stakedBalance = await rewardPoolInstance.totalSupply()
+
+            // Inspect data
+            const lastTimeRewardApplicable = await rewardPoolInstance.lastTimeRewardApplicable()
+            const rewardPerToken = await rewardPoolInstance.rewardPerToken()
+            console.log(`rewardBalance: ${rewardPoolBalance.toString()}`)
+            console.log(`stakedBalance: ${stakedBalance.toString()}`)
+            console.log(`lastTimeRewardApplicable: ${lastTimeRewardApplicable.toString()}`)
+            console.log(`rewardPerToken: ${rewardPerToken.toString()}`)
+            //
+
+            expect(rewardPoolBalance.toString()).to.be.eq(toBN(11E18).toString())
+            expect(stakedBalance.toString()).to.be.eq(toBN(10E18).toString())
+        })
+
+        it('should not accept other coins staking', async () => {
+            const wbnbInstance = await WBNB.deployed()
+            await wbnbInstance.mint(account1, toBN(10e18));
+
+            await wbnbInstance.approve(rewardPoolInstance.address, toBN(5e18), {from: account1})
+            const res = await rewardPoolInstance.stake(toBN(5e18), {from: account1})
+                .catch((err: Error) => err)
+            // Get BALLE balance on contract
+            const rewardPoolBalance = await balleInstance.balanceOf(rewardPoolInstance.address)
+            // Get staked BALLE tokens
+            const stakedBalance = await rewardPoolInstance.totalSupply()
+            
+            expect(res)
+                .to.be.an.instanceOf(Error)
+                .and.to.have.property('reason')
+            expect(rewardPoolBalance.toString()).to.be.eq(toBN(11E18).toString())
+            expect(stakedBalance.toString()).to.be.eq(toBN(10E18).toString())
+        })
+
+        it('should respect allowance limit', async () => {
+            const res = await rewardPoolInstance.stake(toBN(5e18))
+                .catch((err: Error) => err)
+            
+            expect(res)
+                .to.be.an.instanceOf(Error)
+                .and.to.have.property('reason')
+        })
+
+        it('should accept additional staking', async () => {
+
+            await balleInstance.approve(rewardPoolInstance.address, toBN(5e18))
+            await rewardPoolInstance.stake(toBN(5e18))
+            // Get BALLE balance on contract
+            const rewardPoolBalance = await balleInstance.balanceOf(rewardPoolInstance.address)
+            // Get staked BALLE tokens
+            const stakedBalance = await rewardPoolInstance.totalSupply()
+            const userBalance = await rewardPoolInstance.balanceOf(owner)
+            
+            expect(rewardPoolBalance.toString()).to.be.eq(toBN(16E18).toString())
+            expect(stakedBalance.toString()).to.be.eq(toBN(15E18).toString())
+            expect(userBalance.toString()).to.be.eq(toBN(10E18).toString())
+        })
+
+    })
+
 })
