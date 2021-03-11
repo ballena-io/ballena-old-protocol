01ec01c4f6873ab7704261597926e05fbd5a92b7
jffernandez <jffernandez@fenix-es.com>
2021-03-11 20:11:47 +0100

    Fix import path

--- contracts/vaults/BalleRewardedVaultV2.sol
+++ contracts/vaults/BalleRewardedVaultV2.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "../interfaces/IStrategy.sol";
 import "../interfaces/IVaultRewardPool.sol";
 
